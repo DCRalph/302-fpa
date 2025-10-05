e9e2f8c9670cb31e4fde4224e7223d4a708d6634
Technoblade <william@williamgiles.co.nz>
2025-10-06 10:54:51 +1300

    member header

--- src/app/member-dashboard/layout.tsx
+++ src/app/member-dashboard/layout.tsx
@@ -3,28 +3,43 @@ import { AppSideBar } from "~/components/app-side-bar";
 import { Separator } from "~/components/ui/separator";
 import { cabin } from "~/components/fonts";
 import { UserButton } from "@stackframe/stack";
+import { MemberHeader } from "~/components/memberHeader";
 
 export default function MemberDashboardLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <SidebarProvider>
-            <AppSideBar />
-            <main className="p-6 w-full">
-                <div className="flex justify-between">
-                    <div className="flex items-center gap-4 pl-3 ">
-                        <SidebarTrigger size={"icon"} className="" />
-                        <Separator orientation="vertical" className="!h-5" />
-                        <h1 className={`${cabin.className} text-2xl`}>Dashboard</h1>
-                    </div>
-                    <div>
-                        <UserButton showUserInfo={true} />
-                    </div>
-                </div>
-                {children}
-            </main>
-        </SidebarProvider>
-    )
+  //     return (
+  //         <SidebarProvider>
+  //             <AppSideBar />
+  //             <main className="p-6 w-full">
+  //                 <div className="flex justify-between">
+  //                     <div className="flex items-center gap-4 pl-3 ">
+  //                         <SidebarTrigger size={"icon"} className="" />
+  //                         <Separator orientation="vertical" className="!h-5" />
+  //                         <h1 className={`${cabin.className} text-2xl`}>Dashboard</h1>
+  //                     </div>
+  //                     <div>
+  //                         <UserButton showUserInfo={true} />
+  //                     </div>
+  //                 </div>
+  //                 {children}
+  //             </main>
+  //         </SidebarProvider>
+  //     )
+  // }
+  return (
+    <SidebarProvider>
+      <div className="flex min-h-screen w-full">
+        <AppSideBar />
+        <div className="flex flex-col flex-1 overflow-x-hidden">
+          <MemberHeader />
+          <main className="">
+            {children}
+          </main>
+        </div>
+      </div>
+    </SidebarProvider >
+  );
 }
--- src/components/app-side-bar.tsx
+++ src/components/app-side-bar.tsx
@@ -1,6 +1,13 @@
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
+"use client";
+
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator } from "./ui/sidebar";
 import { LayoutDashboard, Calendar, BookOpen, FileText, User, GraduationCap } from "lucide-react";
 import Link from "next/link";
+import { useAuth } from "~/lib/auth";
+import { usePathname } from "next/navigation";
+import { useSidebar } from "./ui/sidebar";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 
 // Menu items
 
@@ -33,37 +40,67 @@ const menuItems = [
 ]
 
 export function AppSideBar() {
-    return (
-        <Sidebar>
-            <SidebarHeader>
-                <div className="p-2 text-lg font-bold flex items-center gap-2">
-                    <div className="grid size-11 place-items-center rounded-2xl bg-gradient-to-br from-gradient-blue from-25% via-gradient-purple via-50% to-gradient-red to-75% to text-primary-foreground shadow-lg group-hover:shadow-xl transition-shadow duration-300">
-                        <GraduationCap size={24} className="drop-shadow-sm" />
-                    </div>
-                    <div>
-                        <p>FPA Conference</p>
-                        <p className="text-sm text-muted-foreground font-medium">{new Date().getFullYear()}</p>
-                    </div>
-                </div>
-            </SidebarHeader>
-            <SidebarGroup>
-                <SidebarGroupLabel>Application</SidebarGroupLabel>
-                <SidebarContent>
-                    <SidebarMenu>
-                        {menuItems.map((item) => (
-                            <SidebarMenuItem key={item.title}>
-                                <SidebarMenuButton asChild>
-                                    <Link href={item.url}>
-                                        <item.icon />
-                                        <span>{item.title}</span>
-                                    </Link>
-                                </SidebarMenuButton>
-                            </SidebarMenuItem>
-                        ))}
-                    </SidebarMenu>
-                </SidebarContent>
-            </SidebarGroup>
+    const { dbUser, stackUser } = useAuth();
+    const pathname = usePathname();
+    const {
+        state,
+        // toggleSidebar,
+        isMobile,
+        openMobile,
+        setOpenMobile,
+    } = useSidebar();
+    const [isCollapsed, setIsCollapsed] = useState(state === "collapsed");
 
-        </Sidebar>
+    useEffect(() => {
+        setIsCollapsed(state === "collapsed");
+    }, [state]);
+
+    const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => setMounted(true), []);
+
+    if (!dbUser) return null;
+
+    const isActive = (path: string) => {
+        if (path === "/admin") return pathname === "/admin";
+        return pathname?.startsWith(path) ?? false;
+    };
+    return (
+        <div className="relative h-screen">
+
+            <Sidebar collapsible="icon" className="fixed top-0 left-0">
+                <SidebarHeader className="relative flex items-center justify-between h-16">
+                    <div className="p-2 text-lg font-bold flex items-center gap-2">
+                        <div className="grid size-11 place-items-center rounded-2xl bg-gradient-to-br from-gradient-blue from-25% via-gradient-purple via-50% to-gradient-red to-75% to text-primary-foreground shadow-lg group-hover:shadow-xl transition-shadow duration-300">
+                            <GraduationCap size={24} className="drop-shadow-sm" />
+                        </div>
+                        <div>
+                            <p>FPA Conference</p>
+                            <p className="text-sm text-muted-foreground font-medium">{new Date().getFullYear()}</p>
+                        </div>
+                    </div>
+                </SidebarHeader>
+                <SidebarSeparator />
+                <SidebarContent className="no-scrollbar overflow-x-hidden overflow-y-scroll!">
+                    <SidebarGroup>
+                        <SidebarGroupLabel>Application</SidebarGroupLabel>
+                        {/* <SidebarContent> */}
+                        <SidebarMenu>
+                            {menuItems.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <Link href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroup>
+                </SidebarContent>
+
+            </Sidebar>
+        </div>
     )
 }
