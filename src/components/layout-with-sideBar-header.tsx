741c64f52b0e96060ad29d785117ac516b9cdb3e
William Giles <william@williamgiles.co.nz>
2025-11-07 11:15:45 +1300

    fix rounding on mobile

--- src/components/layout-with-sideBar-header.tsx
+++ src/components/layout-with-sideBar-header.tsx
@@ -12,10 +12,10 @@ export function LayoutWithSideBarHeader({ children, sidebar, header: header }: L
     <SidebarProvider>
       <div className="bg-sidebar flex h-screen w-full">
         {sidebar}
-        <div className="flex flex-1 flex-col w-full overflow-x-hidden md:mt-2 rounded-tl-xl bg-page-background">
+        <div className="flex flex-1 flex-col w-full overflow-x-hidden md:mt-2 rounded-none sm:rounded-tl-xl bg-page-background">
           {/* <div className="shadow-glass"> */}
-            {header}
-            {children}
+          {header}
+          {children}
           {/* </div> */}
         </div>
       </div>
